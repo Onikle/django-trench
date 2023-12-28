e53622b9912afcb1564006df700b0706d28c4125
mpty1952 <takao.m1952@gmail.com>
2023-12-28 19:43:33 +0900

    fix error response shape

--- trench/responses.py
+++ trench/responses.py
@@ -13,33 +13,19 @@ class DispatchResponse(Response):
 
 
 class SuccessfulDispatchResponse(DispatchResponse):
-    def __init__(
-        self, details: str, status: str = HTTP_200_OK, *args, **kwargs
-    ) -> None:
-        super().__init__(
-            data={self._FIELD_DETAILS: details}, status=status, *args, **kwargs
-        )
+    def __init__(self, details: str, status: str = HTTP_200_OK, *args, **kwargs) -> None:
+        super().__init__(data={self._FIELD_DETAILS: details}, status=status, *args, **kwargs)
 
 
 class FailedDispatchResponse(DispatchResponse):
-    def __init__(
-        self, details: str, status: str = HTTP_422_UNPROCESSABLE_ENTITY, *args, **kwargs
-    ) -> None:
-        super().__init__(
-            data={self._FIELD_DETAILS: details}, status=status, *args, **kwargs
-        )
+    def __init__(self, details: str, status: str = HTTP_422_UNPROCESSABLE_ENTITY, *args, **kwargs) -> None:
+        super().__init__(data={self._FIELD_DETAILS: details}, status=status, *args, **kwargs)
 
 
 class ErrorResponse(Response):
-    _FIELD_ERROR = "error"
-
-    def __init__(
-        self,
-        error: MFAValidationError,
-        status: str = HTTP_400_BAD_REQUEST,
-        *args,
-        **kwargs
-    ) -> None:
-        super().__init__(
-            data={self._FIELD_ERROR: str(error)}, status=status, *args, **kwargs
-        )
+    def __init__(self, error: MFAValidationError, status: str = HTTP_400_BAD_REQUEST, *args, **kwargs) -> None:
+        if isinstance(error.detail, list):
+            detail = error.detail[0]
+        else:
+            detail = error.detail
+        super().__init__(data={"code": getattr(detail, "code", "none"), "message": str(detail)}, status=status, *args, **kwargs)
